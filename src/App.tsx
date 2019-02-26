992ed9744cb8988c592c1b3f7885d9e77a1400e2
kobakakzu0429 <kobakazu10@gmail.com>
2019-02-26 15:49:49 +0900

    introduce styled-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styled from "styled-components";
+
 import logo from './logo.svg';
-import './App.css';
 
 const App =() => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+  <Wrapper>
+    <Header>
+      <Logo src={logo} className="App-logo" alt="logo" />
       <p>
         Hello, React World !
       </p>
-    </header>
-  </div>
+    </Header>
+  </Wrapper>
 );
+
+const Wrapper = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const Logo = styled.img`
+  animation: App-logo-spin infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from "styled-components";
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const StyledGlobal = createGlobalStyle`
+  *,
+  *:before,
+  *:after {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -o-box-sizing: border-box;
+    -ms-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+  `;
+
+ReactDOM.render(
+  <React.Fragment>
+    <App />
+    <StyledGlobal />
+  </React.Fragment>,
+  document.getElementById('root')
+);
