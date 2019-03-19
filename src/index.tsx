db9cf0bbbe9d4655aa4a514c66821fae9dd52127
kobakakzu0429 <kobakazu10@gmail.com>
2019-03-19 13:21:47 +0900

    add

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
-import App from "./App";
+
+import IndexPage from "./pages/Index";
+import AdminPage from "./pages/Admin";
+
+import { Provider } from "./RootContext";
 
 const StyledGlobal = createGlobalStyle`
   *,
@@ -15,16 +20,40 @@ const StyledGlobal = createGlobalStyle`
   }
 
   html, body {
+    margin: 0;
+    padding: 0;
+    color: #333;
+    font-family: 'Noto Sans JP', sans-serif;
     -webkit-app-region: drag;
     -webkit-user-select: none;
+    -moz-user-select: none;
+    -khtml-user-select: none;
+    -ms-user-select: none;
     background: rgba(0, 0, 0, 0);
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
-  `;
+`;
+
+const ws = new WebSocket("ws://127.0.0.1:5001");
+
+ws.onopen = (e: any) => {
+  console.log("Opened !");
+};
 
 ReactDOM.render(
-  <React.Fragment>
-    <App />
-    <StyledGlobal />
-  </React.Fragment>,
+  <Provider
+    value={{
+      ws
+    }}
+  >
+    <BrowserRouter>
+      <React.Fragment>
+        <Route exact path="/" component={IndexPage} />
+        <Route path="/admin" component={AdminPage} />
+        <StyledGlobal />
+      </React.Fragment>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
